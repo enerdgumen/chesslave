f24f4abad38f563c6e4dc3529b5ae43d8a9ceb39
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2016-06-19 13:54:29 +0200

    Refactor BotMoverTest to test flipped boards
    Tested both flipped and unflipped boards. At the moment, board flipping code is not really clean, but moving flipped and unflipped behaviours in two distinct tests doesn't work due to parallel threads execution.

--- src/test/java/io/chesslave/hands/BotMoverTest.java
+++ src/test/java/io/chesslave/hands/BotMoverTest.java
@@ -3,13 +3,13 @@ package io.chesslave.hands;
 import static org.junit.Assume.assumeThat;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-import io.chesslave.visual.BoardImage;
 import io.chesslave.eyes.Images;
 import io.chesslave.eyes.Vision;
 import io.chesslave.eyes.sikuli.SikuliScreen;
 import io.chesslave.eyes.sikuli.SikuliVision;
 import io.chesslave.hands.sikuli.SikuliPointer;
 import io.chesslave.model.Square;
+import io.chesslave.visual.BoardImage;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +17,6 @@ import org.junit.runners.Parameterized;
 
 import java.awt.Desktop;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.Collection;
@@ -25,73 +24,111 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class BotMoverTest {
     private static final String DIR_IMAGES = "/images/";
-    private static final String DIR_PIECE_SET = DIR_IMAGES + "set1/";
     private static final String DIR_HANDS = DIR_IMAGES + "hands/";
-    private static final String IMAGE_INITIAL_BOARD = DIR_PIECE_SET + "initial-board.png";
+    private static final String DIR_VISUAL = DIR_IMAGES + "visual/";
+    private static final String IMAGE_UNFLIPPED_BOARD = DIR_VISUAL + "board.png";
+    private static final String IMAGE_FLIPPED_BOARD = DIR_VISUAL + "flipped-board.png";
     private static final String IMAGE_RESET_BUTTON = DIR_HANDS + "chesscom-reset-button.png";
+    private static final String IMAGE_FLIP_BUTTON = DIR_HANDS + "chesscom-flip-board-button.png";
     private static final String CHESS_BOARD_WEB_PAGE = "https://www.chess.com/analysis-board-editor";
 
     @Parameterized.Parameter
     public BaseBotMover botMover;
 
     @Parameterized.Parameter(value = 1)
-    public Rectangle resetButtonArea;
+    public Point resetButtonPoint;
+
+    @Parameterized.Parameter(value = 2)
+    public Point flipButtonPoint;
+
+    private Pointer pointer;
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
-        BoardImage board = null;
-        Rectangle button = null;
+        BoardImage unflippedBoard = null;
+        BoardImage flippedBoard = null;
+        Point resetPoint = null;
+        Point flipPoint = null;
         try {
             Desktop.getDesktop().browse(new URI(CHESS_BOARD_WEB_PAGE));
             // 5 seconds to open the browser
-            Thread.sleep(5000);
+            Thread.sleep(5000L);
 
             final SikuliScreen screen = new SikuliScreen();
-            final Vision.Recogniser recogniser = new SikuliVision().recognise(screen.captureAll());
+            Vision.Recogniser recogniser = new SikuliVision().recognise(screen.captureAll());
 
-            board = recogniser.bestMatch(Images.read(IMAGE_INITIAL_BOARD))
-                    .map(match -> new BoardImage(match.image(), match.region().getLocation()))
+            unflippedBoard = recogniser.bestMatch(Images.read(IMAGE_UNFLIPPED_BOARD))
+                    .map(match -> new BoardImage(match.image(), match.region().getLocation(), false))
                     .getOrElse((BoardImage) null);
-            button = recogniser.bestMatch(Images.read(IMAGE_RESET_BUTTON))
+            resetPoint = recogniser.bestMatch(Images.read(IMAGE_RESET_BUTTON))
                     .map(Vision.Match::region)
-                    .getOrElse((Rectangle) null);
+                    .map(rect -> new Point((int) rect.getCenterX(), (int) rect.getCenterY()))
+                    .getOrElse((Point) null);
+
+            // flip board
+            flipPoint = recogniser.bestMatch(Images.read(IMAGE_FLIP_BUTTON))
+                    .map(Vision.Match::region)
+                    .map(rect -> new Point((int) rect.getCenterX(), (int) rect.getCenterY()))
+                    .getOrElse((Point) null);
+            if (flipPoint != null) {
+                new SikuliPointer().click(flipPoint);
+            }
+            // wait webapp's response
+            Thread.sleep(500);
+            // recapture screen after flip
+            recogniser = new SikuliVision().recognise(screen.captureAll());
+
+            flippedBoard = recogniser.bestMatch(Images.read(IMAGE_FLIPPED_BOARD))
+                    .map(match -> new BoardImage(match.image(), match.region().getLocation(), true))
+                    .getOrElse((BoardImage) null);
         } catch (Exception e) {
             // ignore
         }
 
         return Arrays.asList(new Object[][]{
-                {board != null ? new ClickBotMover(board) : null, button},
-                {board != null ? new DragBotMover(board) : null, button}
+                {unflippedBoard != null ? new ClickBotMover(unflippedBoard) : null, resetPoint, flipPoint},
+                {unflippedBoard != null ? new DragBotMover(unflippedBoard) : null, resetPoint, null},
+                {flippedBoard != null ? new ClickBotMover(flippedBoard) : null, resetPoint, flipPoint},
+                {flippedBoard != null ? new DragBotMover(flippedBoard) : null, resetPoint, null}
         });
     }
 
     @Before
     public void setUp() throws Exception {
         assumeThat(botMover, notNullValue());
-        assumeThat(resetButtonArea, notNullValue());
+        assumeThat(resetButtonPoint, notNullValue());
 
-        // reset board position after each test
-        Point point = new Point((int) resetButtonArea.getCenterX(), (int) resetButtonArea.getCenterY());
-        new SikuliPointer().click(point);
+        pointer = new SikuliPointer();
     }
 
     @Test
-    public void sicilianTest() throws Exception {
+    public void someSimpleOpenings() throws Exception {
+        // flip board when necessary
+        if (flipButtonPoint != null) {
+            pointer.click(flipButtonPoint);
+        }
+
+        // reset board position after each sequence
+        pointer.click(resetButtonPoint);
+
+        // Sicilian
         botMover.move(Square.of("e2"), Square.of("e4"));
         botMover.move(Square.of("c7"), Square.of("c5"));
         botMover.move(Square.of("g1"), Square.of("f3"));
         botMover.move(Square.of("d7"), Square.of("d6"));
         botMover.move(Square.of("d2"), Square.of("d4"));
         botMover.move(Square.of("c5"), Square.of("d4"));
-    }
 
-    @Test
-    public void spanishTest() throws Exception {
+        pointer.click(resetButtonPoint);
+
+        // Spanish
         botMover.move(Square.of("e2"), Square.of("e4"));
         botMover.move(Square.of("e7"), Square.of("e5"));
         botMover.move(Square.of("g1"), Square.of("f3"));
         botMover.move(Square.of("b8"), Square.of("c6"));
         botMover.move(Square.of("f1"), Square.of("b5"));
         botMover.move(Square.of("g8"), Square.of("f6"));
+        botMover.move(Square.of("e1"), Square.of("g1"));
+        botMover.move(Square.of("f8"), Square.of("e7"));
     }
 }
