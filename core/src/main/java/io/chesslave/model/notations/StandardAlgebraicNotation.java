4a2587fd023b323f6dc20407c96ec729a62d3514
Mauro Rocchi <maurocchi@gmail.com>
2016-07-31 19:34:09 +0200

    Make StandardAlgebraicNotation methods more coherent

--- core/src/main/java/io/chesslave/model/notations/StandardAlgebraicNotation.java
+++ core/src/main/java/io/chesslave/model/notations/StandardAlgebraicNotation.java
@@ -7,7 +7,7 @@ import io.chesslave.model.Piece.Type;
 import javaslang.collection.HashMap;
 import javaslang.collection.Map;
 import javaslang.collection.Set;
-import java.util.Optional;
+import javaslang.control.Option;
 import static io.chesslave.model.Movements.Regular;
 import static javaslang.API.Case;
 import static javaslang.API.Match;
@@ -24,7 +24,6 @@ public class StandardAlgebraicNotation implements MoveNotation {
     private static final String CHECK_SYMBOL = "+";
     private static final String CHECKMATE_SYMBOL = "#";
     private static final Map<Type, String> PIECE_NAMES = HashMap.of(
-            Type.PAWN, "",
             Type.KNIGHT, "N",
             Type.BISHOP, "B",
             Type.ROOK, "R",
@@ -38,44 +37,44 @@ public class StandardAlgebraicNotation implements MoveNotation {
                 Case(instanceOf(ShortCastling.class), mv -> {
                     final StringBuilder notation = new StringBuilder();
                     notation.append(SHORT_CASTLING);
-                    notation.append(checkNotation(move, position, mv.color.opponent()).orElse(""));
+                    notation.append(checkNotation(move, position, mv.color.opponent()).getOrElse(""));
                     return notation.toString();
                 }),
                 Case(instanceOf(LongCastling.class), mv -> {
                     final StringBuilder notation = new StringBuilder();
                     notation.append(LONG_CASTLING);
-                    notation.append(checkNotation(move, position, mv.color.opponent()).orElse(""));
+                    notation.append(checkNotation(move, position, mv.color.opponent()).getOrElse(""));
                     return notation.toString();
                 }),
                 Case(instanceOf(Regular.class), mv -> {
                     final Piece piece = position.at(mv.from).get();
                     final StringBuilder notation = new StringBuilder();
-                    notation.append(pieceNotation(piece));
-                    notation.append(disambiguatingSymbol(mv, position));
-                    notation.append(captureNotation(mv, position));
+                    notation.append(pieceNotation(piece).getOrElse(""));
+                    notation.append(disambiguatingSymbol(mv, position).getOrElse(""));
+                    notation.append(captureNotation(mv, position).getOrElse(""));
                     notation.append(mv.to.name());
-                    notation.append(checkNotation(move, position, piece.color.opponent()).orElse(""));
+                    notation.append(checkNotation(move, position, piece.color.opponent()).getOrElse(""));
                     return notation.toString();
                 })
         );
     }
 
-    private String disambiguatingSymbol(Regular move, Position position) {
+    private Option<String> disambiguatingSymbol(Regular move, Position position) {
         final Piece piece = position.at(move.from).get();
         final Set<Square> ambiguousSquares = ambiguousSquares(piece, move, position);
         if (!ambiguousSquares.isEmpty()) {
             if (ambiguousSquares.size() == 1) {
-                return ambiguousSquares.head().col != move.from.col ?
-                        String.valueOf((char) ('a' + move.from.col)) :
-                        String.valueOf(move.from.row + 1);
+                return Option.of(ambiguousSquares.head().col != move.from.col
+                        ? String.valueOf((char) ('a' + move.from.col))
+                        : String.valueOf(move.from.row + 1));
             } else {
-                return move.from.name();
+                return Option.of(move.from.name());
             }
         } else if (move.enPassant ||
                 Type.PAWN.equals(piece.type) && position.at(move.to).isDefined()) {
-            return String.valueOf((char) ('a' + move.from.col));
+            return Option.of(String.valueOf((char) ('a' + move.from.col)));
         }
-        return "";
+        return Option.none();
     }
 
     private Set<Square> ambiguousSquares(Piece piece, Regular move, Position position) {
@@ -86,20 +85,22 @@ public class StandardAlgebraicNotation implements MoveNotation {
                 .map(mv -> mv.from);
     }
 
-    private String pieceNotation(Piece piece) {
-        return PIECE_NAMES.apply(piece.type);
+    private Option<String> pieceNotation(Piece piece) {
+        return PIECE_NAMES.get(piece.type);
     }
 
-    private String captureNotation(Movements.Regular move, Position position) {
-        return move.enPassant || position.at(move.to).isDefined() ? CAPTURE_SYMBOL : "";
+    private Option<String> captureNotation(Movements.Regular move, Position position) {
+        return move.enPassant || position.at(move.to).isDefined()
+                ? Option.of(CAPTURE_SYMBOL)
+                : Option.none();
     }
 
-    private Optional<String> checkNotation(Move move, Position position, Color opponentColor) {
+    private Option<String> checkNotation(Move move, Position position, Color opponentColor) {
         final Position resultingPosition = move.apply(position);
         if (Rules.isKingSafe(resultingPosition, opponentColor)) {
-            return Optional.empty();
+            return Option.none();
         }
         final boolean checkmate = Rules.allMoves(resultingPosition, opponentColor).isEmpty();
-        return Optional.of(checkmate ? CHECKMATE_SYMBOL : CHECK_SYMBOL);
+        return Option.of(checkmate ? CHECKMATE_SYMBOL : CHECK_SYMBOL);
     }
 }
