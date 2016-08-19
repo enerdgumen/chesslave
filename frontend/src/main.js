f5fca5750365c1a7c5bd989a20bf600aaf828976
Mauro Rocchi <maurocchi@gmail.com>
2016-08-20 00:29:50 +0200

    Resize app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,8 @@ const {app, BrowserWindow} = require('electron')
 let win
 
 function createWindow () {
-    win = new BrowserWindow({width: 800, height: 600})
     win.loadURL(`file://${__dirname}/index.html`)
+    win = new BrowserWindow({width: 400, height: 800})
     win.webContents.openDevTools()
     win.on('closed', () => {
         win = null
