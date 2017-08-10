206e802fc8be66facc006607de62c1df7d09930d
Mauro Rocchi <maurocchi@gmail.com>
2017-08-10 15:07:31 +0200

    Customize bootstrap style with less

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp')
 const electron = require('electron-connect').server.create()
 
+gulp.task('less', function() {
+    const less = require('gulp-less')
+    return gulp.src('src/app/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('src/app'))
+})
+
 gulp.task('serve', function() {
     electron.start()
     // Currently the restarter works bad!
     // gulp.watch('src/main.js', electron.restart)
-    gulp.watch(['src/app/**/*'], electron.reload)
+    gulp.watch('src/app/*.less', ['less'])
+    gulp.watch('src/app/**/*', electron.reload)
 })
+
+gulp.task('default', ['less'])
